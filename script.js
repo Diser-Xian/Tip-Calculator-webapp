c4ff0e3820354ed79adbb4c3dd0623cbfdf57d23
Diser-Xian <cedrcickzabala19@gmail.com>
2026-04-18 16:37:38 +0800

    structured html, style css, init js var

---
+++ script.js
@@ -0,0 +1,14 @@
+const billAmount = document.getElementById("bill_display_input");
+const percentBtn = document.querySelectorAll("percent_btn");
+const customTip = document.getElementById("custom_tip_input");
+
+const warningPnum = document.getElementById("warning");
+
+const personQ = document.getElementById("person_num");
+
+const tipAmountPerson = document.getElementById("tip_amount_per_person");
+const totalTipAmountPerson = document.getElementById("total_per_person");
+
+const resetBtn = document.getElementById("reset");
+
+
