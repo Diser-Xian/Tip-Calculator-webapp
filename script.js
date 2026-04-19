80a8fdfb67f80fb1670134909cb9b0837c908c42
Diser-Xian <cedrcickzabala19@gmail.com>
2026-04-19 22:45:53 +0800

    added js functionality, added more css hover effect

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const billAmount = document.getElementById("bill_display_input");
-const percentBtn = document.querySelectorAll("percent_btn");
+const percentBtn = document.querySelectorAll("#percent_btn");
 const customTip = document.getElementById("custom_tip_input");
 
 const warningPnum = document.getElementById("warning");
@@ -11,4 +11,84 @@ const totalTipAmountPerson = document.getElementById("total_per_person");
 
 const resetBtn = document.getElementById("reset");
 
+let inputBill = 0;
+let percentValue = 0;
+let numOfPerson = 0;
 
+billAmount.addEventListener("input", BillAmount);
+
+percentBtn.forEach(percent_btn => {
+    percent_btn.addEventListener("click", calPercent);
+});
+
+customTip.addEventListener("input", addToPercentVal);
+personQ.addEventListener("input", people);
+
+resetBtn.addEventListener("click", clear);
+
+
+
+
+
+
+
+
+
+
+
+// functions initialization
+function BillAmount() {
+    inputBill = parseFloat(billAmount.value);
+    console.log(inputBill);
+
+    displayResult();
+
+}
+
+function calPercent(event) {
+    var decimal = parseFloat(event.target.textContent) / 100;
+    percentValue = decimal * inputBill;
+    console.log(percentValue.toFixed(2));
+}
+
+
+function addToPercentVal(event) {
+    percentValue = parseFloat(event.target.value);
+    console.log(percentValue);
+}
+
+function people(event) {
+    numOfPerson = parseInt(event.target.value);
+    console.log(numOfPerson);
+
+    displayResult();
+}
+
+
+function displayResult() {
+    if (numOfPerson > 0 && inputBill > 0) {
+        tipAmountPerson.textContent = (percentValue / numOfPerson).toFixed(2);
+        totalTipAmountPerson.textContent = ((inputBill + percentValue) / numOfPerson).toFixed(2);
+    }
+    else if (numOfPerson > 0 || inputBill > 0) {
+        resetBtn.style.backgroundColor = "#26c0ab";
+    }
+    
+    else{
+        resetBtn.style.backgroundColor = " hsl(183, 100%, 20%)";
+    }
+}
+
+
+function clear(event) {
+   
+    inputBill = 0;
+    percentValue = 0;
+    numOfPerson = 0;
+
+    billAmount.value = "";
+    personQ.value = "";
+    customTip.value = "";
+    tipAmountPerson.textContent = "0.00";
+    totalTipAmountPerson.textContent = "0.00";
+}
